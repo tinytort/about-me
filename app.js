e1086f4860cb31e2ef482bb0fe14d18e680deab4
Haley Tortorella <hdtort@gmail.com>
2017-05-02 14:38:40 -0700

    added js file

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+var age = prompt('Is Haley 24?');
+var snacks = prompt('Is it true that Haley hates snacks?');
+var snakes = prompt('Does Haley love snakes?');
+var tattoo = prompt('Does Haley have a tattoo of a tooth?');
+var dog = prompt('Is Haley obsessed with her dog, Benson Wigglepuff?');
