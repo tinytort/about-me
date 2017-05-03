e54c0dc97d6e5efa01f256ce05d72ad0556217e9
Haley Tortorella <hdtort@gmail.com>
2017-05-02 20:52:36 -0700

    new js folder

---
+++ java/app.js
@@ -0,0 +1,58 @@
+'use strict';
+var name = prompt('Hey there! What is your name?');
+    alert('Welcome, ' + name + '! I hope you are ready for some silliness!');
+var age = prompt('Is Haley 24?');
+if (age.toUpperCase() === 'YES' || age.toUpperCase() ===  'Y') {
+        alert('I sure am!');
+    } 
+else if (age.toUpperCase() === 'NO' || age.toUpperCase() === 'N') {
+     alert('Do you think I look old or something?');
+    }
+else{
+   alert('That is not a valid Response. Please respond with YES/NO or Y/N');
+    }
+console.log('Is Haley 24? ' + age)
+var snacks = prompt('Is it true that Haley hates snacks?');
+if (snacks.toLowerCase() === 'no' || snacks.toLowerCase() === 'n') {
+   alert('Of course not! She is a very hungry human.');
+}
+else if (snacks.toLowerCase() === 'yes' || snacks.toLowerCase() === 'y') {
+    alert('Ummm excuse me?');
+}
+else{
+    alert('That is not a valid response. Please respond with YES/NO or Y/N');
+}
+console.log('Is it true that Haley hates snacks? ' + snacks);
+var snakes = prompt('Does Haley love snakes?');
+if (snakes.toUpperCase() === 'NO' || snakes.toUpperCase() === 'N') {
+    alert('She hates them with every fiber of her being.');
+}
+else if (snakes.toUpperCase() === 'YES' || snakes.toUpperCase() === 'Y') {
+    alert('You could not be more wrong.');
+}
+else {
+    alert('That is not a valid response. Please respond with YES/NO or Y/N');
+}
+console.log('Does Haley love snakes? ' + snakes);
+var tattoo = prompt('Does Haley have a tattoo of a tooth?');
+if (tattoo.toLowerCase() === 'yes' || tattoo.toLowerCase() === 'y') {
+    alert('Truuuuuuuu')
+}
+else if(tattoo.toLowerCase() === 'no' || tattoo.toLowerCase() === 'n') {
+    alert('Oh, yeah she actually does...');
+}
+else{
+    alert('That is not a valid response. Please respond with YES/NO or Y/N');
+}
+console.log('Does Haley have a tattoo of a tooth? ' + tattoo);
+var dog = prompt('Is Haley obsessed with her dog, Benson Wigglepuff?');
+if (dog.toUpperCase() === 'YES' || dog.toUpperCase() === 'Y') {
+    alert('Yeah, and it is a little weird...');
+}
+else if(dog.toUpperCase() === 'NO' || dog.toUpperCase() === 'N') {
+    alert('She is hella obsessed.');
+}
+else{
+    alert('That is not a valid response. Please respond with YES/NO or Y/N.');
+}
+console.log('Is Haley obsessed with her dog, Benson Wigglepuff? ' + dog);
