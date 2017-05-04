17d424b09868d8ac0932282856019e52fdfbc6ca
Haley Tortorella <hdtort@gmail.com>
2017-05-03 17:05:40 -0700

    correct answer tally

--- script/app.js
+++ script/app.js
@@ -1,9 +1,13 @@
 'use strict';
+
 var name = prompt('Hey there! What is your name?');
 alert('Welcome, ' + name + '! I hope you are ready for some silliness!');
+
+var correctAnswer = 0
 var age = prompt('Is Haley 24?');
 if (age.toUpperCase() === 'YES' || age.toUpperCase() === 'Y') {
     alert('I sure am!');
+    correctAnswer += 1;
 }
 else if (age.toUpperCase() === 'NO' || age.toUpperCase() === 'N') {
     alert('Do you think I look old or something?');
@@ -12,9 +16,11 @@ else {
     alert('That is not a valid Response. Please respond with YES/NO or Y/N');
 }
 console.log('Is Haley 24? ' + age)
+
 var snacks = prompt('Is it true that Haley hates snacks?');
 if (snacks.toLowerCase() === 'no' || snacks.toLowerCase() === 'n') {
     alert('Of course not! She is a very hungry human.');
+    correctAnswer += 1;
 }
 else if (snacks.toLowerCase() === 'yes' || snacks.toLowerCase() === 'y') {
     alert('Ummm excuse me?');
@@ -23,9 +29,11 @@ else {
     alert('That is not a valid response. Please respond with YES/NO or Y/N');
 }
 console.log('Is it true that Haley hates snacks? ' + snacks);
+
 var snakes = prompt('Does Haley love snakes?');
 if (snakes.toUpperCase() === 'NO' || snakes.toUpperCase() === 'N') {
     alert('She hates them with every fiber of her being.');
+    correctAnswer += 1;
 }
 else if (snakes.toUpperCase() === 'YES' || snakes.toUpperCase() === 'Y') {
     alert('You could not be more wrong.');
@@ -34,9 +42,11 @@ else {
     alert('That is not a valid response. Please respond with YES/NO or Y/N');
 }
 console.log('Does Haley love snakes? ' + snakes);
+
 var tattoo = prompt('Does Haley have a tattoo of a tooth?');
 if (tattoo.toLowerCase() === 'yes' || tattoo.toLowerCase() === 'y') {
-    alert('Truuuuuuuu')
+    alert('Truuuuuuuu');
+    correctAnswer += 1;
 }
 else if (tattoo.toLowerCase() === 'no' || tattoo.toLowerCase() === 'n') {
     alert('Oh, yeah she actually does...');
@@ -45,9 +55,11 @@ else {
     alert('That is not a valid response. Please respond with YES/NO or Y/N');
 }
 console.log('Does Haley have a tattoo of a tooth? ' + tattoo);
+
 var dog = prompt('Is Haley obsessed with her dog, Benson Wigglepuff?');
 if (dog.toUpperCase() === 'YES' || dog.toUpperCase() === 'Y') {
     alert('Yeah, and it is a little weird...');
+    correctAnswer += 1;
 }
 else if (dog.toUpperCase() === 'NO' || dog.toUpperCase() === 'N') {
     alert('She is hella obsessed.');
@@ -58,11 +70,12 @@ else {
 console.log('Is Haley obsessed with her dog, Benson Wigglepuff? ' + dog);
 
 var popsicle = 3;
-for (numberOfGuesses = 0; numberOfGuesses < 4; numberOfGuesses++) {
+for (var numberOfGuesses = 0; numberOfGuesses < 4; numberOfGuesses++) {
     var popsicleAnswer = prompt('How many popsicles did Haley eat yesterday?')
     popsicleAnswer = parseInt(popsicleAnswer)
     if (popsicleAnswer === popsicle) {
         alert('She sure did!')
+        correctAnswer += 1;
         break;
     } else if (popsicleAnswer > popsicle) {
         alert('Too High')
@@ -80,9 +93,12 @@ for (numberOfGuesses = 0; numberOfGuesses < 6; numberOfGuesses++) {
         alert('Nope! Try again!')
     } else {
         alert('That\'s right!')
+        correctAnswer += 1;
         break;
     }
 }
 if(numberOfGuesses === 6) {
     alert('Sorry, the possible answers were pugs, puggles or labs!')
 }
+
+console.log ('Your score is ' + correctAnswer  + ' out of 7, ' + name + '. Thanks for playing!');
